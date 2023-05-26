747f204cc8acfae185363b0d5c32840af0c35a89
ab-5860 <kmrabhi9987@gmail.com>
2023-05-26 12:36:41 +0530

    delete expense event added

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -10,12 +10,21 @@ const ExpenseItem = (props) => {
     console.log('Clicked !!!!');
   }
 
+  const deleteExpenseHandler = () => {
+    console.log('Expense deleted!');
+    // Remove the expense item from the DOM
+    const expenseItem = document.getElementById(`expense-${props.id}`);
+    if (expenseItem) {
+      expenseItem.remove();
+    }
+  }
 
   return (
     <Card className = "expense-item">
       <ExpenseDate date = {props.date}/>
         <ExpenseDetails amount = {props.amount} location = {props.location} title = {props.title} />
       <button onClick = {(clickHandler) }>Change Title</button>
+      <button onClick={deleteExpenseHandler}>Delete Expense</button>
     </Card>
   );
 }
